b8b22fcf5337d0899012c2febd97d79f95d504d0
wych(witch) <wych@wychwit.ch>
2024-05-09 10:32:36 -0500

    Add version planning

--- AradiaBot/Program.cs
+++ AradiaBot/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * This is where I'm gonna put a bunch of stuff yeahyeah
+ * 
+ * Version 0.1 features:
+ * --- Support for slash commands and prefix commands (along with other options such as menu for some commands)
+ * ------ Prefix configurable in config (will be ? as I develop and transition)
+ * --- Nickname system (for use in pings and quotes)
+ * --- User Settings (to store nickname, game scores, and other settings)
+ * --- Pings (Improved)
+ * ------ Can automatically add Nickname, can be refused
+ * ------ Pings are stored in user settings
+ * --- Quotes (Improved)
+ * ------ Uses ID -> Nickname system so people can quote via reacting 
+ * ------ Still has an option to hardcode the nickname
+ * ------ Better editing
+ * ------ Option to react to a message to quote, or rightclick and use the menu to quote
+ * 
+ * Version 0.2 features:
+ * --- AZ game (Improved)
+ * ------ Has two modes, hard and normal, with Normal using the top 1000(?) common english words
+ * ------ [Maybe later add a mode for CHN and JPN? And other languages as people suggest]
+ * ------ Singleplayer mode where you can have your own game going where others can't join in. 
+ * --------- Only one multiplayer mode can be ongoing at a time, and each user can have only one single player game
+ * ------ AZ is the overall gamemode, and it loads different lists dynamically (which will have the possibility for other languages)
+ * --- PokeAZ
+ * ------ Would actually be a sublist under AZ game!
+ * 
+ * Version 0.3 features:
+ * --- Undercut and its versions
+ * 
+ * Version 0.4 features:
+ * --- (Might spinoff into another bot) Improve the DM tools including the dm posting
+ * 
+ */
+
 Console.WriteLine("Hello, World!");
