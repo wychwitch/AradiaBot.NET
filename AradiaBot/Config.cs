bd85f1cd6cc71e315b9c1b735e1ca1954e8762f7
wychwitch <wych@wychwit.ch>
2024-05-11 17:00:11 -0500

    create config class

---
+++ AradiaBot/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AradiaBot
+{
+    internal class Config
+    {
+        public string token {  get; set; }
+        public List<ulong> guildIds { get; set; }
+
+        
+    }
+}
