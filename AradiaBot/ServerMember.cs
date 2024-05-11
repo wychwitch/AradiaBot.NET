2f6c6ccf78c9e24154a17cca28733b184c95ed6e
wychwitch <wych@wychwit.ch>
2024-05-11 18:55:32 -0500

    Create GetName Func

--- AradiaBot/ServerMember.cs
+++ AradiaBot/ServerMember.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
+using Discord;
+
 namespace AradiaBot
 {
     internal class ServerMember
@@ -13,6 +15,18 @@ namespace AradiaBot
         public ulong Id { get; set; }
         public string NickName { get; set; }
         public ServerMemberSettings Settings { get; set; }
+
+        public string GetName() {
+            if (Settings.UseNickname && NickName != null)
+            {
+                return NickName;
+            } 
+            else 
+            { 
+                return MentionUtils.MentionUser(Id); 
+            }
+        
+        }
 
         
 
