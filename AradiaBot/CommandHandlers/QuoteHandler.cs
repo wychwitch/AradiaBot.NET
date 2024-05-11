b5f3bf4fb4a5649fe86bb6dda2f140bfa23f867c
wychwitch <wych@wychwit.ch>
2024-05-11 18:21:26 -0500

    add quotehandlers

--- AradiaBot/CommandHandlers/QuoteHandler.cs
+++ AradiaBot/CommandHandlers/QuoteHandler.cs
@@ -11,7 +11,7 @@ namespace AradiaBot.CommandHandlers
 {
     internal class QuoteHandler
     {
-        public static async Task ProcessSlashCommand(SocketSlashCommand command)
+        public static async Task ProcessSlashCommand(Database database,SocketSlashCommand command)
         {
             var commandName = command.Data.Options.First().Name;
             Console.WriteLine(commandName);
@@ -22,7 +22,47 @@ namespace AradiaBot.CommandHandlers
                 case "add":
                     await AddCommand(command);
                     break;
+                case "random":
+                    await RandomQuote(database, command);
+                    break;
+
             }
+        }
+
+
+        public static async Task ProcessMessageCommand(Database database, SocketMessageCommand command) 
+        {
+            var commandName = command.CommandName;
+
+            switch(commandName)
+            {
+                case "Quote Message":
+                    await AddCommandMessage(database, command);
+                    break;
+            }
+        }
+        public static async Task RandomQuote(Database database, SocketSlashCommand command, int num = 1)
+        {
+            var quotes = database.Quotes;
+            var random = new Random();
+            var randomNum = random.Next(quotes.Count);
+
+            var quote = quotes[randomNum];
+            Console.WriteLine($"Quote: {quote}");
+
+            await command.ModifyOriginalResponseAsync(x => x.Content = $"{quote.QuoteBody}");
+
+        }
+        private static async Task AddCommandMessage(Database database, SocketMessageCommand command)
+        {
+            var author = command.Data.Message.Author;
+            var quoter = command.User;
+            var body = command.Data.Message.Content;
+
+            await AddQuote(database, author, body, quoter);
+
+            await command.RespondAsync("Quo0ted!");
+
         }
 
         private static async Task AddCommand(SocketSlashCommand command)
@@ -50,6 +90,8 @@ namespace AradiaBot.CommandHandlers
 
         private static async Task AddStaticQuote(SocketSlashCommand command) { }
 
+        
+
         private static async Task DelCommand(SocketSlashCommand command)
         {
             throw new NotImplementedException();
@@ -59,6 +101,15 @@ namespace AradiaBot.CommandHandlers
         {
             throw new NotImplementedException();
         }
+
+        private static async Task AddQuote(Database database, IUser author,  string body, IUser quoter)
+        {
+            Quote quote = new Quote(author, quoter, body);
+
+            database.Quotes.Add(quote);
+            database.SaveData();
+        }
+
     }
     
    
