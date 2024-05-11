e8a30327985a6fae30df5b375c9a085ce9e1201e
wychwitch <wych@wychwit.ch>
2024-05-10 22:35:30 -0500

    servermember constructor

--- AradiaBot/ServerMember.cs
+++ AradiaBot/ServerMember.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Discord;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,11 @@ namespace AradiaBot
         public ulong Id { get; set; }
         public string NickName { get; set; }
         public ServerMemberSettings Settings { get; set; }
+        public ServerMember(IUser user) 
+        { 
+            Id = user.Id;
+            NickName = "";
+            Settings = new();
+        }
     }
 }
