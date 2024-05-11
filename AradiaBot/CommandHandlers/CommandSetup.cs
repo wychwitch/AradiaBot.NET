aea17ade6edf52dc19d1adb4c8c4cfce990fb5d3
wychwitch <wych@wychwit.ch>
2024-05-11 16:59:40 -0500

    Moved Commandhandling to seperate class

---
+++ AradiaBot/CommandHandlers/CommandSetup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+namespace AradiaBot.CommandHandlers
+{
+    internal class CommandSetup
+    {
+        public async static Task RegisterSlashCommandsAsync(DiscordSocketClient client, List<SlashCommandBuilder> slashCommandBuilders, List<ulong> guildIds) {
+            foreach (ulong guildId in guildIds)
+            {
+                var guild = client.GetGuild(guildId);
+
+                foreach (var slashCommand in slashCommandBuilders)
+                {
+                    try
+                    {
+                        await guild.CreateApplicationCommandAsync(slashCommand.Build());
+
+                    }
+                    catch (HttpException exception)
+                    {
+                        // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+                        var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+
+                        // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+                        Console.WriteLine(json);
+                    }
+                }
+            }
+
+        }
+    }
+}
