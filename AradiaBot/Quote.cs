37132e52c271b6860391e6c2a9c5fc36b03f59f6
wychwitch <wych@wychwit.ch>
2024-05-11 18:17:30 -0500

    create quote constructors

--- AradiaBot/Quote.cs
+++ AradiaBot/Quote.cs
@@ -4,17 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
+using Newtonsoft.Json;
 
 namespace AradiaBot
 {
     internal class Quote
     {
-        public ulong Id { get; set; }
-        public ulong Author { get; set; }
+        public string Author { get; set; }
         public ulong Quoter { get; set; }
         public string QuoteBody { get; set; }
 
-        public static Embed QuoteFormatter(List<ServerMember> serverMembers) {
+        [JsonConstructor]
+        public Quote(string author, ulong quoter, string quoteBody)
+        {
+            Author = author;
+            Quoter = quoter;
+            QuoteBody = quoteBody;
+        }
+        public Quote(string author, IUser quoter, string quoteBody)
+        {
+            Author = author;
+            Quoter = quoter.Id;
+            QuoteBody = quoteBody;
+        }
+        public Quote(IUser author, IUser quoter, string quoteBody)
+        {
+            Author = $"{author.Id}";
+            Quoter = quoter.Id;
+            QuoteBody = quoteBody;
+        }
+        public Quote(string author, string quoteBody)
+        {
+            Author = author;
+            Quoter = 0;
+            QuoteBody = quoteBody;
+        }
+
+
+
+        public static Embed QuoteFormatter(Database database) {
             //Todo
 
             EmbedBuilder formattedQuote = new EmbedBuilder().WithDescription("Nothing Is Here Yet");
