e91d11b43dbd1ab97572a8ae319453acec029710
wychwitch <wych@wychwit.ch>
2024-05-10 22:35:57 -0500

    fix constructor and add savedata

--- AradiaBot/Database.cs
+++ AradiaBot/Database.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AradiaBot
 {
     internal class Database
     {
-        public List<ServerMember> Members = new();
+        public List<ServerMember> Members { get; set; }
         public Dictionary<string, Dictionary<ulong, int>> GameScores { get; set; }
-        public Quotes Quotes { get; set; }
+        public List<Quote> Quotes { get; set; }
 
         public Database()
         {
             GameScores = new Dictionary<string, Dictionary<ulong, int>>();
             GameScores["az"] = new Dictionary<ulong, int>();
 
-            Quotes = new Quotes();
+            Quotes = new();
 
+            Members = new List<ServerMember>();
+
+
+        }
+        public void SaveData()
+        {
+            string jsonString = JsonSerializer.Serialize(this);
+            Console.Write(jsonString);
+        }
+        public static void LoadData()
+        {
+            throw new NotImplementedException();
         }
     }
 }
